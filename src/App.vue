5c4e068f05fdcc35dd64d2cc9771d9cfbc68a00b
FabioVasi <vasifabio96@gmail.com>
2023-09-22 15:48:11 +0200

    add connention and partials

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <script>
 
+  import HeaderComics from "./components/HeaderComics.vue";
+  import LogoComics from "./components/LogoComics.vue";
+  import MainComics from "./components/MainComics.vue";
+  import FooterComics from "./components/FooterComics.vue";
+  
+  export default {
+    components: {
+      HeaderComics,
+      LogoComics,
+      MainComics,
+      FooterComics
+    }
+  }
+
 </script>
 
 <template>
-  
+
+  <HeaderComics/>
+  <LogoComics/>
+  <MainComics/>
+  <FooterComics/>
+
 </template>
 
 <style>
