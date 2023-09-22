17db49cd1ca855a282575262b2ae1b64367a2a1d
FabioVasi <vasifabio96@gmail.com>
2023-09-22 18:53:18 +0200

    add footer

--- src/components/FooterComics.vue
+++ src/components/FooterComics.vue
@@ -10,7 +10,21 @@
 
 <template>
 
-    <footer id="footer_comics">
+    <footer id="footer_comics" class="d-flex align-items-center">
+        <div class="container p-0 d-flex justify-content-between align-items-center">
+            <a href="#">
+                <button class="text-white">SIGN-UP NOW!</button>
+            </a>
+            <div class="d-flex justify-content-between align-items-center">
+                <a class="pe-3 text-decoration-none" href=""><h3>FOLLOW US</h3></a>
+                <a class="pe-3" href="#"><img src="../assets/img/footer-facebook.png" alt=""></a>
+                <a class="pe-3" href="#"><img src="../assets/img/footer-twitter.png" alt=""></a>
+                <a class="pe-3" href="#"><img src="../assets/img/footer-youtube.png" alt=""></a>
+                <a class="pe-3" href="#"><img src="../assets/img/footer-pinterest.png" alt=""></a>
+                <a class="pe-3" href="#"><img src="../assets/img/footer-periscope.png" alt=""></a>
+            </div>
+            
+        </div>
 
     </footer>
 
@@ -22,7 +36,16 @@
 
     #footer_comics{
         background-color: $bg_dcfooter;
-        height: 200px;
+        height: 150px;
+        margin-top: -100px;
+        position: relative;
+        a button{
+            background-color: $bg_dcfooter;
+            text-decoration: none;
+            border: 3px solid $color_dclogo;
+            font-size: 20px;
+            padding: 1rem 1.50rem;
+        }
     }
 
 </style>
