762c291d9ad90753dee47842e7bfe8b481f530ae
FabioVasi <vasifabio96@gmail.com>
2023-09-22 17:52:55 +0200

    add list

--- src/components/HeaderComics.vue
+++ src/components/HeaderComics.vue
@@ -14,45 +14,47 @@
 
 <template>
 
-  <header id="header_comics" class="d-flex justify-content-evenly align-items-center">
-    <LogoComics/>
-    <nav class="navbar navbar-expand">
-        <ul class="nav navbar-nav">
-          <li class="nav-item fw-bold fs-4">
-            <a class="nav-link" href="#">CHARACFTERS</a>
-          </li>
-          <li class="nav-item fw-bold fs-4">
-            <a class="nav-link active" href="#" aria-current="page">COMICS<span class="visually-hidden">(current)</span></a>
-          </li>
-          <li class="nav-item fw-bold fs-4">
-            <a class="nav-link" href="#">MOVIES</a>
-          </li>
-          <li class="nav-item fw-bold fs-4">
-            <a class="nav-link" href="#">TV</a>
-          </li>
-          <li class="nav-item fw-bold fs-4">
-            <a class="nav-link" href="#">GAMES</a>
-          </li>
-          <li class="nav-item fw-bold fs-4">
-            <a class="nav-link" href="#">COLLECTIBLES</a>
-          </li>
-          <li class="nav-item fw-bold fs-4">
-            <a class="nav-link" href="#">VIDEOS</a>
-          </li>
-          <li class="nav-item fw-bold fs-4">
-            <a class="nav-link" href="#">FANS</a>
-          </li>
-          <li class="nav-item fw-bold fs-4">
-            <a class="nav-link" href="#">NEWS</a>
-          </li>
-          <li class="nav-item fw-bold fs-4">
-            <a class="nav-link" href="#">SHOP</a>
-          </li>
-        </ul>
-    </nav>
+  <header id="header_comics" class="d-flex align-items-center">
+
+    <div class="container p-0 d-flex justify-content-between align-items-center">
+      <LogoComics/>
+      <nav class="navbar navbar-expand">
+          <ul class="nav navbar-nav">
+            <li class="nav-item fw-bold fs-4">
+              <a class="nav-link" href="#">CHARACFTERS</a>
+            </li>
+            <li class="nav-item fw-bold fs-4">
+              <a class="nav-link active" href="#" aria-current="page">COMICS<span class="visually-hidden">(current)</span></a>
+            </li>
+            <li class="nav-item fw-bold fs-4">
+              <a class="nav-link" href="#">MOVIES</a>
+            </li>
+            <li class="nav-item fw-bold fs-4">
+              <a class="nav-link" href="#">TV</a>
+            </li>
+            <li class="nav-item fw-bold fs-4">
+              <a class="nav-link" href="#">GAMES</a>
+            </li>
+            <li class="nav-item fw-bold fs-4">
+              <a class="nav-link" href="#">COLLECTIBLES</a>
+            </li>
+            <li class="nav-item fw-bold fs-4">
+              <a class="nav-link" href="#">VIDEOS</a>
+            </li>
+            <li class="nav-item fw-bold fs-4">
+              <a class="nav-link" href="#">FANS</a>
+            </li>
+            <li class="nav-item fw-bold fs-4">
+              <a class="nav-link" href="#">NEWS</a>
+            </li>
+            <li class="nav-item fw-bold fs-4">
+              <a class="nav-link" href="#">SHOP</a>
+            </li>
+          </ul>
+      </nav>
+    </div>
 
   </header>
-
 
 </template>
 
--- src/components/MainComics.vue
+++ src/components/MainComics.vue
@@ -11,11 +11,14 @@
 <template>
 
   <main id="main_comics">
+
     <div class="container p-0">
         <h1 class="text-white pt-5 pb-5 m-0">Content goes here</h1>
     </div>
+
     <div class="merchandise">
-        <div class="container pt-5 pb-5 d-flex justify-content-between align-items-center">
+        <div class="container ps-0 pe-0 pt-5 pb-5 d-flex justify-content-between align-items-center">
+
             <a class="text-white text-decoration-none" href="#">
                 <img height="70px" src="../assets/img/buy-comics-digital-comics.png" alt="">
                 <span class="ps-3 fs-5">DIGITAL COMICS</span>
@@ -36,17 +39,69 @@
                 <img height="50px" src="../assets/img/buy-dc-power-visa.svg" alt="">
                 <span class="ps-3 fs-5">DIGITAL COMICS</span>
             </a>
-            
-            
-            
-            
-            
+    
         </div>
     </div>
+
+    <div id="heroes" class="d-flex">
+        <div class="container p-0 d-flex justify-content-between">
+            <div class="d-flex justify-content-evenly">
+                <ul class="lists list-unstyled pt-5 pb-5 pe-5">
+                <li>
+                    <h3 class="text-white pt-3 pb-3">DC COMICS</h3>
+                </li>
+                <li class="fs-5 fw-medium pt-1 pb-1">Characters</li>
+                <li class="fs-5 fw-medium pt-1 pb-1">Comics</li>
+                <li class="fs-5 fw-medium pt-1 pb-1">Movies</li>
+                <li class="fs-5 fw-medium pt-1 pb-1">TV</li>
+                <li class="fs-5 fw-medium pt-1 pb-1">Games</li>
+                <li class="fs-5 fw-medium pt-1 pb-1">Videos</li>
+                <li class="fs-5 fw-medium pt-1 pb-1">News</li>
+                <li>
+                    <h3 class="text-white pt-3 pb-3">SHOP</h3>
+                </li>
+                <li class="fs-5 fw-medium pt-1 pb-1">Shop DC</li>
+                <li class="fs-5 fw-medium pt-1 pb-1">Shop DC Collectibles</li>
+            </ul>
+            <ul class="lists list-unstyled pt-5 pb-5 pe-5">
+                <li>
+                    <h3 class="text-white pt-3 pb-3">DC</h3>
+                </li>
+                <li class="fs-5 fw-medium pt-1 pb-1">Terms Of Use</li>
+                <li class="fs-5 fw-medium pt-1 pb-1">Privacy policy (New)</li>
+                <li class="fs-5 fw-medium pt-1 pb-1">Ad Choices</li>
+                <li class="fs-5 fw-medium pt-1 pb-1">Advertising</li>
+                <li class="fs-5 fw-medium pt-1 pb-1">Jobs</li>
+                <li class="fs-5 fw-medium pt-1 pb-1">Subscriptions</li>
+                <li class="fs-5 fw-medium pt-1 pb-1">Talent Workshops</li>
+                <li class="fs-5 fw-medium pt-1 pb-1">CPSC Certificates</li>
+                <li class="fs-5 fw-medium pt-1 pb-1">Ratings</li>
+                <li class="fs-5 fw-medium pt-1 pb-1">Shop Help</li>
+                <li class="fs-5 fw-medium pt-1 pb-1">Contact Us</li>
+            </ul>
+            <ul class="lists list-unstyled pt-5 pb-5">
+                <li>
+                    <h3 class="text-white pt-3 pb-3">SITES</h3>
+                </li>
+                <li class="fs-5 fw-medium pt-1 pb-1">DC</li>
+                <li class="fs-5 fw-medium pt-1 pb-1">MAD Magazine</li>
+                <li class="fs-5 fw-medium pt-1 pb-1">DC Kids</li>
+                <li class="fs-5 fw-medium pt-1 pb-1">DC Universe</li>
+                <li class="fs-5 fw-medium pt-1 pb-1">DC Power Visa</li>
+            </ul>
+            </div>
+            
+            <img src="../assets/img/dc-logo-bg.png" alt="">
+        </div>
+        
+    </div>
+
   </main>
 
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+
+    
 
 </style>
