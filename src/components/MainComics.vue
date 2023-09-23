c22f3cb9772288a06c37c71180f0d0d9c7337952
FabioVasi <vasifabio96@gmail.com>
2023-09-23 15:32:02 +0200

    add lists

--- src/components/MainComics.vue
+++ src/components/MainComics.vue
@@ -25,6 +25,84 @@
                 image: 'src/assets/img/buy-dc-power-visa.svg'
             },
             
+        ],
+        list: '',
+        lists: [
+            {
+                link: 'Characters'
+            },
+            {
+                link: 'Comics'
+            },
+            {
+                link: 'Movies'
+            },
+            {
+                link: 'TV'
+            },
+            {
+                link: 'Games'
+            },
+            {
+                link: 'Videos'
+            },
+            {
+                link: 'News'
+            }            
+        ],
+        info: '',
+        information: [
+            {
+                about: 'Terms Of Use'
+            },
+            {
+                about: 'Privacy policy (New)'
+            },
+            {
+                about: 'Ad Choices'
+            },
+            {
+                about: 'Advertising'
+            },
+            {
+                about: 'Jobs'
+            },
+            {
+                about: 'Subscriptions'
+            },
+            {
+                about: 'Talent Workshops'
+            },
+            {
+                about: 'CPSC Certificates'
+            },
+            {
+                about: 'Ratings'
+            },
+            {
+                about: 'Shop Help'
+            },
+            {
+                about: 'Contact Us'
+            }
+        ],
+        site: '',
+        sites: [
+            {
+                source: 'DC'
+            },
+            {
+                source: 'MAD Magazine'
+            },
+            {
+                source: 'DC Kids'
+            },
+            {
+                source: 'DC Universe'
+            },
+            {
+                source: 'DC Power Visa'
+            }
         ]
       }
     }
@@ -41,12 +119,10 @@
 
         <div class="merchandise position-relative">
             <div class="container ps-0 pe-0 pt-5 pb-5 d-flex justify-content-between align-items-center">
-
                 <a v-for="card in cards" class="text-white text-decoration-none" href="#">
                     <img style="height: 80px;" :src="card.image" alt="">
                     <span class="ps-3">{{card.name}}</span>
                 </a>
-        
             </div>
         </div>
 
@@ -54,47 +130,29 @@
             <div class="container p-0 d-flex justify-content-between">
                 <div class="d-flex justify-content-evenly">
                     <ul class="lists list-unstyled pt-4 m-0 pb-5 pe-5">
-                    <li>
-                        <h3 class="text-white pt-3 pb-3 m-0">DC COMICS</h3>
-                    </li>
-                    <li class="fw-medium pt-1 pb-1">Characters</li>
-                    <li class="fw-medium pt-1 pb-1">Comics</li>
-                    <li class="fw-medium pt-1 pb-1">Movies</li>
-                    <li class="fw-medium pt-1 pb-1">TV</li>
-                    <li class="fw-medium pt-1 pb-1">Games</li>
-                    <li class="fw-medium pt-1 pb-1">Videos</li>
-                    <li class="fw-medium pt-1 pb-1">News</li>
-                    <li>
-                        <h3 class="text-white pt-3 pb-3 m-0">SHOP</h3>
-                    </li>
-                    <li class="fw-medium pt-1 pb-1">Shop DC</li>
-                    <li class="fw-medium pt-1 pb-1">Shop DC Collectibles</li>
+                        <li>
+                            <h3 class="text-white pt-3 pb-3 m-0">DC COMICS</h3>
+                        </li>                    
+                        <li v-for="list in lists" class="fw-medium pt-1 pb-1">{{list.link}}</li>
+                        <li>
+                            <h3 class="text-white pt-3 pb-3 m-0">SHOP</h3>
+                        </li>
+                        <li class="fw-medium pt-1 pb-1">Shop DC</li>
+                        <li class="fw-medium pt-1 pb-1">Shop DC Collectibles</li>
                     </ul>
+
                     <ul class="lists list-unstyled pt-4 m-0 pb-5 pe-5">
                         <li>
                             <h3 class="text-white pt-3 pb-3 m-0">DC</h3>
                         </li>
-                        <li class="fw-medium pt-1 pb-1">Terms Of Use</li>
-                        <li class="fw-medium pt-1 pb-1">Privacy policy (New)</li>
-                        <li class="fw-medium pt-1 pb-1">Ad Choices</li>
-                        <li class="fw-medium pt-1 pb-1">Advertising</li>
-                        <li class="fw-medium pt-1 pb-1">Jobs</li>
-                        <li class="fw-medium pt-1 pb-1">Subscriptions</li>
-                        <li class="fw-medium pt-1 pb-1">Talent Workshops</li>
-                        <li class="fw-medium pt-1 pb-1">CPSC Certificates</li>
-                        <li class="fw-medium pt-1 pb-1">Ratings</li>
-                        <li class="fw-medium pt-1 pb-1">Shop Help</li>
-                        <li class="fw-medium pt-1 pb-1">Contact Us</li>
+                        <li v-for="info in information" class="fw-medium pt-1 pb-1">{{info.about}}</li>
                     </ul>
+
                     <ul class="lists list-unstyled pt-4 m-0 pb-5 pe-5">
                         <li>
                             <h3 class="text-white pt-3 pb-3 m-0">SITES</h3>
                         </li>
-                        <li class="fw-medium pt-1 pb-1">DC</li>
-                        <li class="fw-medium pt-1 pb-1">MAD Magazine</li>
-                        <li class="fw-medium pt-1 pb-1">DC Kids</li>
-                        <li class="fw-medium pt-1 pb-1">DC Universe</li>
-                        <li class="fw-medium pt-1 pb-1">DC Power Visa</li>
+                        <li v-for="site in sites" class="fw-medium pt-1 pb-1">{{site.source}}</li>
                     </ul>
                 </div>            
                 <img class="dc-logo-bg" src="../assets/img/dc-logo-bg.png" alt="">
